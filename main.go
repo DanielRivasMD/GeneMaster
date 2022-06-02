fb2b494565dee3d5a857d49d168eb8ab0d2e2a00
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-02 14:30:27 +0200

    patch repository path

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package main
 
-import "GeneMaster/cmd"
+import "github.com/DanielRivasMD/GeneMaster/cmd"
 
 func main() {
 	cmd.Execute()
